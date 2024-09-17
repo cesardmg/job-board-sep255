132d25062d100c56f4a946cf94b71901cb671eb6
Cesar <95641536+cesa2149@users.noreply.github.com>
2024-09-17 11:03:56 -0600

    controller actions

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,47 @@
 class JobsController < ApplicationController
+  def index
+    @jobs = Job.all.order(created_at: :desc)
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to @job, notice: "Job was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to @job, notice: "Job was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_url, notice: "Job was successfully destroyed."
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description, :company, :location)
+  end
 end
